e709e1ffff6d18649d15539c6e141712046eb977
Alan Vinicius Silva <alanvinicios@gmail.com>
2021-01-08 17:01:09 -0300

    Auto git push

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-document.querySelectorAll(".ytp-ce-element").forEach(e => {
-    e.style.display = "none";
-});
+function hideCards(){
+    document.querySelectorAll(".ytp-ce-element").forEach(e => {
+        e.style.display = "none";
+    });
+};
+
+
+setTimeout(hideCards, 5000);
